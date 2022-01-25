bb319fde2d34f799793817c2e5c236bff0870f22
andreazani77 <andreazani77@users.noreply.github.com>
2022-01-25 07:08:11 +0100

    Fine sezione 8

--- src/MyCouse/Controllers/CoursesController.cs
+++ src/MyCouse/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCouse.Models.Services.Application;
+using MyCouse.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +13,18 @@ namespace MyCouse.Controllers
         public IActionResult Index()
         {
             //cercherà una views contenuta nella cartella views\courses\index
-            return View();
+            var courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetCourses();
+            ViewData["Title"] = "Catalogo dei corsi"; //Verrà utilizzato dalla view di layout per impostare il titolo
+            return View(courses);
         }
 
-        public IActionResult Detail(string id)
+        public IActionResult Detail(int id)
         {
-            return View();
+            var courseService = new CourseService();
+            CourseDetailViewModel viewModel = courseService.GetCourse(id);
+            ViewData["Title"] = viewModel.Title; //Verrà utilizzato dalla view di layout per impostare il titolo
+            return View(viewModel);
         }
 
         public IActionResult Search(string title)
---
+++ src/MyCouse/Models/ViewModels/CourseDetailViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MyCouse.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyCouse.Models.ViewModels
+{
+    
+    //Eredita tuttii campi di CourseView model in più ne aggiunge altri
+    //tra cui il list delle lezioni che è di tipo LessonViewModel
+    public class CourseDetailViewModel:CourseViewModel
+    {
+        public string Description { get; set; }
+        public List<LessonViewModel> Lessons { get; set; }
+
+        public TimeSpan TotalCourseDuration()
+        {
+            //get => TimeSpan.FromSeconds(Lessons?.Sum(l => l.Duration.TotalSeconds) ?? 0);
+            TimeSpan total = TimeSpan.FromSeconds(0);
+            foreach(LessonViewModel lesson in Lessons)
+            {
+                total = total + lesson.Duration;
+            }
+
+            return total;
+
+        }
+
+    }
+
+
+}
